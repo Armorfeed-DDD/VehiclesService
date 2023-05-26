cb43ec2679012744e972d85517c23744540acd21
MaxTer45 <106483409+MaxTer43@users.noreply.github.com>
2023-05-26 00:12:07 -0500

    feat: added vehicle id getter

--- src/main/java/com/armorfeed/api/vehicles/domain/Vehicles.java
+++ src/main/java/com/armorfeed/api/vehicles/domain/Vehicles.java
@@ -1,8 +1,18 @@
 package com.armorfeed.api.vehicles.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @AllArgsConstructor
+@Getter
+@Table(name = "vehicles")
 public class Vehicles {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
