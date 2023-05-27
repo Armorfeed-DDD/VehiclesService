d16665d1166a7b0bda2b5121961348e319b6cbe7
Alvaro <93229518+Alvaro2003j@users.noreply.github.com>
2023-05-27 00:10:30 -0500

    update vehicles insert in the branch

--- src/main/java/com/armorfeed/api/vehicles/controller/VehicleController.java
+++ src/main/java/com/armorfeed/api/vehicles/controller/VehicleController.java
@@ -2,8 +2,10 @@ package com.armorfeed.api.vehicles.controller;
 
 
 import com.armorfeed.api.vehicles.domain.entities.Vehicle;
+import com.armorfeed.api.vehicles.resources.UpdateResource;
 import com.armorfeed.api.vehicles.services.VehiclesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,5 +27,10 @@ public class VehicleController {
     public Vehicle Save(@RequestBody Vehicle vehicle){
         vehiclesService.Save(vehicle);
         return vehicle;
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<?> updateVehicle(@PathVariable Long vehicleId, @RequestBody UpdateResource request){
+        return vehiclesService.updateVehicle(request.getEnterpriseId(), vehicleId, request);
     }
 }
