14c633911832e7dc6729081a28867faaa243cd1e
Miguel445Ar <86012555+Miguel445Ar@users.noreply.github.com>
2023-06-30 23:11:52 -0500

    feature: added validation annotations in update resource

--- src/main/java/com/armorfeed/api/vehicles/resources/UpdateResource.java
+++ src/main/java/com/armorfeed/api/vehicles/resources/UpdateResource.java
@@ -5,27 +5,44 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
 import java.util.Date;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateResource {
-
+    @NotNull
+    @NotBlank
+    @NotEmpty
     private String brand;
 
+    @NotNull
+    @NotBlank
+    @NotEmpty
     private String license_plate;
 
+    @NotNull
+    @NotBlank
+    @NotEmpty
     private String model;
 
+    @NotNull
     private Long year;
 
+    @NotNull
+    @NotBlank
+    @NotEmpty
     private String Vehicle_type;
 
+    @NotNull
     private Date maintenance_date;
 
+    @NotNull
     private Long enterpriseId;
 
 }
